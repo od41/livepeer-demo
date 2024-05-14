333716f3fc0d590829502e530529a08bb0273699
Odafe Aror <odafe41@gmail.com>
2024-05-14 18:26:47 +0000

    update query and display data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import logo from './logo.svg'
 import './App.css'
 import { useQuery } from '@apollo/client'
-import { ExampleQueryDocument } from '../.graphclient'
+import { TranscodersQueryDocument, TranscodersQueryQuery as TranscodersQuery, Pool, Maybe, Transcoder as TranscoderType } from '../.graphclient'
 
+type Transcoder = Array<(
+  Pick<TranscoderType, 'id' | 'activationRound' | 'deactivationRound' | 'lastActiveStakeUpdateRound' | 'feeShare' | 'rewardCut' | 'totalVolumeUSD' | 'active' | 'totalStake'>
+  & { pools?: Maybe<Array<(
+    { __typename: 'Pool' }
+    & Pick<Pool, 'id' | 'totalStake'>
+    & { delegate: Pick<TranscoderType, 'id'> }
+  )>> }
+)>;
+function TranscoderDisplay({transcoder}: {transcoder: any}) {
+  const {
+    active,
+    deactivationRound,
+    id: account,
+    rewardCut,
+    totalStake,
+    totalVolumeUSD
+  } = transcoder
+  console.log(transcoder)
+  return <div>
+    Status: {active ? "active" : "inactive"}
+    deactivationRound: {deactivationRound}
+    account: {account}
+    rewardCut: {rewardCut}
+    totalStake: {totalStake}
+    totalVolumeUSD: {totalVolumeUSD}
+  </div>
+}
 function App() {
-  const result = useQuery(ExampleQueryDocument)
+  const result = useQuery<TranscodersQuery>(TranscodersQueryDocument)
 
   const { data, loading, error, refetch } = result
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -21,9 +49,15 @@ function App() {
         <fieldset>
           {data && (
             <form>
+              <h1>Transcoders: {data?.protocols[0].numActiveTranscoders}</h1>
+              <br />
               <label>Data</label>
               <br />
-              <textarea value={JSON.stringify(data, null, 2)} readOnly rows={25} />
+              <div>
+                {data?.transcoders.map((transcoder, index) => <TranscoderDisplay key={`trans-${transcoder?.id}`} transcoder={transcoder} />)}
+              </div>
+
+              {/* <textarea value={JSON.stringify(data, null, 2)} readOnly rows={25} /> */}
             </form>
           )}
           {error && (
